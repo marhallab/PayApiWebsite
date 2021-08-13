fa2bb2b150ee29c52ff3ee09e9e508462e267628
med_amine_rhallab <1832012@cegeplimoilou.ca>
2021-08-13 12:16:51 -0400

    Create secondary button, dark mode

---
+++ payapi-web/src/components/SecondaryBtnDark.vue
@@ -0,0 +1,49 @@
+<template>
+  <button :disabled="isDisabled">{{ btnText }}</button>
+</template>
+
+<style scoped>
+button {
+  background: #fbfcfe;
+  border-radius: 25px;
+  border: 0 solid #36536B;
+
+  padding-left: 25px;
+  padding-right: 25px;
+
+  font-family: Public Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 15px;
+  color: #36536B;
+
+  z-index: 3;
+}
+
+button:hover {
+  background: #36536B;
+  color: #fbfcfe;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+    btnText: {
+      type: String,
+    },
+  },
+
+  computed: {},
+
+  methods: {
+    test() {
+      alert("YES");
+    },
+  },
+};
+</script>
