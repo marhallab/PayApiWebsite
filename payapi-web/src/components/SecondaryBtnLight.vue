982cce7a363060933ca20b8714180ae9215df39c
Med Amine Rhallab <70241115+marhallab@users.noreply.github.com>
2021-08-14 15:17:20 -0400

    Create both secondary buttons (#1)
    * Create SecondaryBtn.vue
    
    * Create secondary button, light mode
    
    * Create secondary button, dark mode

---
+++ payapi-web/src/components/SecondaryBtnLight.vue
@@ -0,0 +1,49 @@
+<template>
+  <button :disabled="isDisabled">{{ btnText }}</button>
+</template>
+
+<style scoped>
+button {
+  background: #1b262f;
+  border-radius: 25px;
+  border: 0 solid #fbfcfe;
+
+  padding-left: 25px;
+  padding-right: 25px;
+
+  font-family: Public Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 15px;
+  color: #fbfcfe;
+
+  z-index: 3;
+}
+
+button:hover {
+  background: #fbfcfe;
+  color: #1b262f;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+    btnText: {
+      type: String,
+    },
+  },
+
+  computed: {},
+
+  methods: {
+    test() {
+      alert("YES");
+    },
+  },
+};
+</script>
